42bb544200ba80866f13cbc003fa59f736caea05
Alex Alejandro <eaglesalejandro.aa@gmail.com>
2021-05-16 13:51:52 -0400

    made requaest to Api, results were successful

--- script.js
+++ script.js
@@ -1,9 +1,28 @@
 const form = document.querySelector('[data-form]')
 const inputSearch = document.querySelector('[data-search]') 
+import 'regenerator-runtime/runtime'
 const moviesList = document.querySelector('[data-movies-list]')
 const apiKey = 'a60c16eaddacf852ba0fc28403a21c8b'
 
 const apiUrl = `https://api.themoviedb.org/3/movie/popular?api_key=${apiKey}&language=en-US&page=1`
 
+showMovies(apiUrl)
+function showMovies(url){
+    const fetchData = async () => {
+        const response = await fetch(url)
+        const data = await response.json()
+        const results = data.results
+        console.log(results)
+        results.forEach(result => {
+            const movieEl = document.createElement('div')
+            const movieTitle = document.createElement('h2')
+            const moviePoster = document.createElement('img')
 
-
+            movieTitle.innerText= result.title
+           
+            
+        })
+    }
+    fetchData()
+    
+}
